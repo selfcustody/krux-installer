5484f088b9c21ea297e623dab22dc47868699a05
qlrd <106913782+qlrd@users.noreply.github.com>
2023-03-03 21:14:21 -0300

    addded tests for odudex's bit firmware

---
+++ test/specs/select-version/krux_binaries/bit/002.check-resources.spec.ts
@@ -0,0 +1,58 @@
+import { expect as expectWDIO } from '@wdio/globals'
+import delay from '../../../delay'
+import Main from '../../../../pageobjects/main.page'
+import SelectDevice from '../../../../pageobjects/select-device.page'
+import SelectVersion from '../../../../pageobjects/select-version.page'
+import CheckResources from '../../../../pageobjects/check-resources.page'
+
+// eslint-disable-next-line no-undef
+describe('SelectVersionPage: check if exists \'odudex/krux_binaries\' resources for \'bit\'', () => {
+ 
+  // eslint-disable-next-line no-undef
+  before(async () => { 
+    await Main.selectDeviceButton.click()
+    await delay(1000)
+    await SelectDevice.formArrow.click()
+    await delay(1000)
+    await SelectDevice.list_item_bit.waitForExist() 
+    await delay(1000)
+    await SelectDevice.list_item_bit.click()
+    await delay(1000) 
+    await SelectDevice.formSelectButton.click()
+    await SelectDevice.page.waitForExist({ reverse: true })
+    await Main.page.waitForExist()
+    await Main.selectVersionButton.waitForExist()
+    await Main.selectVersionButton.click()
+    await SelectVersion.cardTitleChecked.waitForExist()  
+    await SelectVersion.cardSubtitleOfficial.waitForExist()  
+    await SelectVersion.cardSubtitleTest.waitForExist()  
+    await SelectVersion.formArrow.waitForExist()
+    await SelectVersion.formBackButton.waitForExist()  
+    await delay(1000)
+    await SelectVersion.formArrow.click()
+    await delay(1000)
+    await SelectVersion.list_item_22_03_0.waitForExist()
+    await SelectVersion.list_item_22_08_0.waitForExist()
+    await SelectVersion.list_item_22_08_1.waitForExist()
+    await SelectVersion.list_item_22_08_2.waitForExist()
+    await SelectVersion.list_item_krux_binaries.waitForExist()
+    await delay(1000) 
+    await SelectVersion.list_item_krux_binaries.click()
+    await delay(1000)
+  })
+
+  // eslint-disable-next-line no-undef
+  it('\'odudex/krux_binaries\' should be selected', async () => {    
+    await expectWDIO(SelectVersion.formSelected).toHaveText('odudex/krux_binaries')
+  })
+
+  // eslint-disable-next-line no-undef
+  it('should check between official or test resources', async () => { 
+    await SelectVersion.formSelectButton.waitForExist()
+    await SelectVersion.formSelectButton.click()  
+    await CheckResources.page.waitForExist() 
+    await expectWDIO(CheckResources.cardTitle).toHaveText('Checking between official or test resources...')
+    await CheckResources.page.waitForExist({ reverse: true }) 
+  })
+
+})

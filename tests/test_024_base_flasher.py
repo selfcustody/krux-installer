888b5e8ab49ab53173db3869e0af4ad654831dab
qlrd <qlrd 106913782+qlrd@users.noreply.github.com>
2024-02-09 15:45:14 -0300

    coveraged more methods on base_flasher tests

--- tests/test_024_base_flasher.py
+++ tests/test_024_base_flasher.py
@@ -11,7 +11,7 @@ class TestBaseFlasher(TestCase):
         "src.utils.flasher.base_flasher.BaseFlasher.device",
         new_callable=PropertyInstanceMock,
     )
-    def test_init_device(self, mock_device, mock_exists):
+    def test_init(self, mock_device, mock_exists):
         mock_device.return_value = "test"
 
         f = BaseFlasher(device="test", root_path="mock")
@@ -19,18 +19,61 @@ class TestBaseFlasher(TestCase):
         mock_exists.assert_has_calls(
             [call("mock"), call("mock/maixpy_test/kboot.kfpkg")]
         )
-
-        self.assertEqual(f.device, "test")
         mock_device.assert_has_calls([call(f, "test"), call(f)])
 
-    @patch(
-        "src.utils.flasher.base_flasher.BaseFlasher.root_path",
-        new_callable=PropertyInstanceMock,
-    )
-    def test_init1_root_path(self, mock_root_path):
-        mock_root_path.return_value = "mock"
+    def test_fail_init_device(self):
+        with self.assertRaises(ValueError) as exc_info:
+            BaseFlasher(device="test", root_path="mock")
 
+        self.assertEqual(str(exc_info.exception), "Invalid device: test")
+
+    def test_init_fail_root_path(self):
+        with self.assertRaises(ValueError) as exc_info:
+            BaseFlasher(device="m5stickv", root_path="mock")
+
+        self.assertEqual(str(exc_info.exception), "Directory mock do not exist")
+
+    @patch("os.path.exists", side_effect=[True, False])
+    def test_init_fail_full_path(self, mock_exists):
+        with self.assertRaises(ValueError) as exc_info:
+            BaseFlasher(device="m5stickv", root_path="mock")
+
+        mock_exists.assert_has_calls(
+            [call("mock"), call("mock/maixpy_m5stickv/kboot.kfpkg")]
+        )
+        self.assertEqual(
+            str(exc_info.exception),
+            "File mock/maixpy_m5stickv/kboot.kfpkg do not exist",
+        )
+
+    @patch("os.path.exists", side_effect=[True, True])
+    def test_get_kboot(self, mock_exists):
         f = BaseFlasher(device="m5stickv", root_path="mock")
 
-        self.assertEqual(f.root_path, "mock")
-        mock_root_path.assert_has_calls([call(f, "mock"), call(f)])
+        mock_exists.assert_has_calls(
+            [call("mock"), call("mock/maixpy_m5stickv/kboot.kfpkg")]
+        )
+
+        self.assertEqual(f.ktool.killProcess, False)
+        self.assertEqual(f.ktool.loader, None)
+        self.assertEqual(f.ktool.print_callback, None)
+
+    @patch("os.path.exists", side_effect=[True, True])
+    def test_get_buffer(self, mock_exists):
+        f = BaseFlasher(device="m5stickv", root_path="mock")
+
+        mock_exists.assert_has_calls(
+            [call("mock"), call("mock/maixpy_m5stickv/kboot.kfpkg")]
+        )
+
+        self.assertEqual(f.buffer.readable(), True)
+
+    @patch("os.path.exists", side_effect=[True, True])
+    def test_has_admin_privileges(self, mock_exists):
+        f = BaseFlasher(device="m5stickv", root_path="mock")
+
+        mock_exists.assert_has_calls(
+            [call("mock"), call("mock/maixpy_m5stickv/kboot.kfpkg")]
+        )
+
+        self.assertEqual(f.has_admin_privilege, False)

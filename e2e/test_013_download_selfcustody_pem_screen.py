425e3dae7d9b27459f8fa13f7b377b73ba44f344
qlrd <qlrddev@gmail.com>
2024-07-21 09:33:09 -0300

    coveraged 100% of download_selfcustody_pem_screen

--- e2e/test_013_download_selfcustody_pem_screen.py
+++ e2e/test_013_download_selfcustody_pem_screen.py
@@ -215,6 +215,58 @@ class TestDownloadSelfcustodyPemScreen(GraphicUnitTest):
 
     @patch.object(EventLoopBase, "ensure_window", lambda x: None)
     @patch("src.app.screens.base_screen.App.get_running_app")
+    def test_update_progress_100_percent(self, mock_get_running_app):
+        attrs = {"get.side_effect": ["en-US.UTF8", "mockdir"]}
+        mock_get_running_app.config = MagicMock()
+        mock_get_running_app.config.configure_mock(**attrs)
+
+        screen = DownloadSelfcustodyPemScreen()
+        self.render(screen)
+
+        # get your Window instance safely
+        EventLoop.ensure_window()
+
+        # do tests
+        with patch.object(screen, "trigger") as mock_trigger, patch.object(
+            screen, "downloader"
+        ) as mock_downloader:
+
+            mock_downloader.destdir = "mockdir"
+            screen.update(
+                name="ConfigKruxInstaller",
+                key="progress",
+                value={"downloaded_len": 210, "content_len": 21},
+            )
+
+            # do tests
+            text_progress = "\n".join(
+                [
+                    "[size=100sp][b]100.00 %[/b][/size]",
+                    "",
+                    "[size=16sp]21 of 21 B[/size]",
+                ]
+            )
+
+            text_info = "mockdir/selfcustody.pem downloaded"
+
+            self.assertEqual(
+                screen.ids["download_selfcustody_pem_screen_progress"].text,
+                text_progress,
+            )
+            self.assertEqual(
+                screen.ids["download_selfcustody_pem_screen_info"].text, text_info
+            )
+
+            # patch assertions
+            mock_get_running_app.assert_has_calls(
+                [call().config.get("locale", "lang")],
+                any_order=True,
+            )
+
+            assert len(mock_trigger.mock_calls) >= 1
+
+    @patch.object(EventLoopBase, "ensure_window", lambda x: None)
+    @patch("src.app.screens.base_screen.App.get_running_app")
     @patch("src.app.screens.download_selfcustody_pem_screen.time.sleep")
     @patch(
         "src.app.screens.download_selfcustody_pem_screen.DownloadSelfcustodyPemScreen.set_screen"

8eba00cca3e51c3b323d746c8e310a20dcdd5b37
joaozinho <105161249+joaozinhom@users.noreply.github.com>
2026-07-20 12:39:17 -0300

    chore: remove dead download, selector and unzip utils with their unit tests (#279)

--- src/utils/flasher/flasher.py
+++ src/utils/flasher/flasher.py
@@ -24,7 +24,7 @@ flasher.py
 """
 
 from collections.abc import Callable
-from src.utils.selector import VALID_DEVICES
+from src.utils.constants import VALID_DEVICES
 from src.utils.flasher.base_flasher import BaseFlasher
 
 
